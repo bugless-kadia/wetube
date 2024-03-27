fd3406d2d0a4fedef2e5a55d9fa5c77f14a75ce5
lehn <laurence.for5@gmail.com>
2024-03-27 18:07:42 +0900

    login add

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,7 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render('404', { pageTitle: 'Video not found.' });
+    return res.status(404).render('404', { pageTitle: 'Video not found.' });
   }
   return res.render('watch', { pageTitle: video.title, video });
 };
@@ -18,10 +18,11 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.render('404', { pageTitle: 'Video not found.' });
+    return res.status(404).render('404', { pageTitle: 'Video not found.' });
   }
   return res.render('edit', { pageTitle: `Edit: ${video.title}`, video });
 };
+
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
@@ -40,6 +41,7 @@ export const postEdit = async (req, res) => {
 export const getUpload = (req, res) => {
   return res.render('upload', { pageTitle: 'Upload Video' });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
@@ -50,7 +52,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect('/');
   } catch (error) {
-    return res.render('upload', {
+    return res.status(400).render('upload', {
       pageTitle: 'Upload Video',
       errorMessage: error._message,
     });
