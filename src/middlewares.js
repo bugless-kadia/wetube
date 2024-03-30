18f0af675cdd58f02cdc804616e6b467e53e617a
lehn <laurence.for5@gmail.com>
2024-03-30 17:15:41 +0900

    github login progress

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
-  console.log('hi', req.session);
+  // console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  console.log(res.locals);
   res.locals.siteName = 'Wetube';
   res.locals.loggedInUser = req.session.user;
   next();
